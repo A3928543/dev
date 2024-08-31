a5cabf66cc6bf9a0c08ec02cf3499e6bbe40c1b7
chuy <aplanacalles@hotmail.com>
2024-08-31 00:54:07 -0600

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/bootstrap-1.0.0.jar
---
+++ src/main/java/com/example/bootstrap/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.bootstrap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // nombre del archivo .html en templates
+    }
+}
